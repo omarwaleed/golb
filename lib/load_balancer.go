a6330c269fba70e4c071bc929bd2f9b91f0c1e22
omarwaleed <omarpersonalprofile@gmail.com>
2023-01-02 09:59:59 +0200

    included sticky sessions in configuration

--- lib/load_balancer.go
+++ lib/load_balancer.go
@@ -11,12 +11,14 @@ type LoadBalancer struct {
 	DomainHosts      map[string][]Host
 	DistributionType DistributionType
 	ForceHTTPS       bool
+	Sticky           bool
 }
 
-func NewLoadBalancer(distributionType DistributionType, forceHTTPS bool) *LoadBalancer {
+func NewLoadBalancer(distributionType DistributionType, forceHTTPS bool, sticky bool) *LoadBalancer {
 	return &LoadBalancer{
 		DomainHosts:      make(map[string][]Host),
 		DistributionType: distributionType,
 		ForceHTTPS:       forceHTTPS,
+		Sticky:           sticky,
 	}
 }
