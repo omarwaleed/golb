c8d0cd14d6a2f39d645ecfb7356751b5230d885f
omarwaleed <omarpersonalprofile@gmail.com>
2023-01-02 10:19:54 +0200

    implemented callable functions for host including healthcheck functions

--- lib/host.go
+++ lib/host.go
@@ -1,10 +1,18 @@
 package lib
 
+import (
+	"errors"
+	"net/http"
+	"strings"
+	"time"
+)
+
 type Host struct {
 	IPAddress           string
 	HealthCheckRoute    string
 	HealthCheckInterval int
 	Status              HostStatus
+	healthCheckTicker   *time.Ticker
 }
 
 type HostStatus string
@@ -15,11 +23,48 @@ const (
 	HostStatusUnknown HostStatus = "unknown"
 )
 
-func NewHost(ipAddress string, healthCheckRoute string, healthCheckInterval int) *Host {
+func NewHost(ipAddress string, healthCheckRoute string, healthCheckInterval int) (*Host, error) {
+	if !strings.HasPrefix("/", healthCheckRoute) {
+		return nil, errors.New("health check route must start with a slash")
+	}
+	if strings.HasPrefix("//", healthCheckRoute) {
+		return nil, errors.New("health check route must not start with two slashes")
+	}
+	if healthCheckInterval < 1 {
+		return nil, errors.New("health check interval must be greater than 0")
+	}
 	return &Host{
 		IPAddress:           ipAddress,
 		HealthCheckRoute:    healthCheckRoute,
 		HealthCheckInterval: healthCheckInterval,
 		Status:              HostStatusUnknown,
+	}, nil
+}
+
+func (h *Host) StartHealthCheck() error {
+	if h.healthCheckTicker != nil {
+		return errors.New("health check already started")
 	}
+	ticker := time.NewTicker(time.Duration(h.HealthCheckInterval) * time.Second)
+	h.healthCheckTicker = ticker
+	go func() {
+		for range ticker.C {
+			_, err := http.DefaultClient.Get(h.IPAddress + h.HealthCheckRoute)
+			if err != nil {
+				h.Status = HostStatusDown
+				continue
+			}
+		}
+	}()
+	return nil
+}
+
+func (h *Host) StopHealthCheck() error {
+	if h.healthCheckTicker == nil {
+		return errors.New("health check not started")
+	}
+	h.healthCheckTicker.Stop()
+	h.healthCheckTicker = nil
+	h.Status = HostStatusUnknown
+	return nil
 }
