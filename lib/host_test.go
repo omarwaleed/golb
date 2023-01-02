47065c1172accd2bd3479b1f02b1dbabf5b6aa81
omarwaleed <omarpersonalprofile@gmail.com>
2023-01-02 23:32:16 +0200

    added basic test suites

---
+++ lib/host_test.go
@@ -0,0 +1,31 @@
+package lib
+
+import "testing"
+
+func TestCreatingNewHost(t *testing.T) {
+	host, err := NewHost("example.com", "/", 1)
+	if err != nil {
+		t.Fatalf("host should be created: %v", err)
+	}
+	// t.Log(*host)
+	if host.IPAddress != "example.com" {
+		t.Error("host ip address should be example.com")
+	}
+	if host.HealthCheckRoute != "/" {
+		t.Error("host health check route should be /")
+	}
+	if host.HealthCheckInterval != 1 {
+		t.Error("host health check interval should be 1")
+	}
+}
+
+func TestHostCreateWithWrongInterval(t *testing.T) {
+	_, err := NewHost("example.com", "/", -1)
+	if err == nil {
+		t.Fatalf("host should have thrown an error")
+	}
+	_, err = NewHost("example.com", "/", 0)
+	if err == nil {
+		t.Fatalf("host should have thrown an error")
+	}
+}
---
+++ lib/load_balancer_test.go
@@ -0,0 +1,55 @@
+package lib
+
+import "testing"
+
+func TestLoadBalancerInitializing(t *testing.T) {
+	lb := NewLoadBalancer(DistributionTypeRoundRobin, false, false)
+	if lb.DistributionType != DistributionTypeRoundRobin {
+		t.Fatal("DistributionType should be round_robin")
+	}
+	if lb.ForceHTTPS != false {
+		t.Fatal("ForceHTTPS should be false")
+	}
+	if lb.Sticky != false {
+		t.Fatal("Sticky should be false")
+	}
+	if len(lb.DomainHosts) != 0 {
+		t.Fatal("DomainHosts should be empty")
+	}
+	if lb.LastHostIndex != 0 {
+		t.Fatal("LastHostIndex should be 0")
+	}
+}
+
+func TestLoadBalancerInitializingWithWrongDistributionType(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fatal("The code did not panic")
+		}
+	}()
+	NewLoadBalancer("wrong", false, false)
+}
+
+func TestDistributionTypeParsing(t *testing.T) {
+	distributionType, err := ParseDistrubutionType("round_robin")
+	if err != nil {
+		t.Fatalf("round_robin DistributionType should be parsed: %v", err)
+	}
+	if distributionType != DistributionTypeRoundRobin {
+		t.Fatal("DistributionType should be round_robin")
+	}
+	distributionType, err = ParseDistrubutionType("random")
+	if err != nil {
+		t.Fatalf("random DistributionType should be parsed: %v", err)
+	}
+	if distributionType != DistributionTypeRandom {
+		t.Fatal("DistributionType should be random")
+	}
+	distributionType, err = ParseDistrubutionType("wrong")
+	if err == nil {
+		t.Fatal("wrong DistributionType should not return an error")
+	}
+	if distributionType != "" {
+		t.Fatal("DistributionType should be empty")
+	}
+}
