4f0ac625d4878819f29652c1876034c7eedd3010
omarwaleed <omarpersonalprofile@gmail.com>
2023-01-03 09:25:01 +0200

    applying main structure for sticky session and logger

--- lib/load_balancer.go
+++ lib/load_balancer.go
@@ -2,7 +2,10 @@ package lib
 
 import (
 	"errors"
+	"io"
+	"log"
 	"sync"
+	"time"
 )
 
 type DistributionType string
@@ -13,15 +16,27 @@ const (
 )
 
 type LoadBalancer struct {
-	DistributionType DistributionType
-	ForceHTTPS       bool
-	Sticky           bool
+	DistributionType           DistributionType
+	ForceHTTPS                 bool
+	Sticky                     bool
+	StickySessionResetInterval int // the amount of seconds to hold a sticky session before an IP address is forgotten
+	StickySessionMemoryMu      sync.RWMutex
+	StickySessionMemory        map[ipAddressWithPrefix]stickySessionMemoryEntry
 
 	DomainHostsMu sync.RWMutex
 	DomainHosts   map[string][]Host
 
 	LastHostIndexMu sync.RWMutex
 	LastHostIndex   int
+
+	LogChan chan LogEntry
+}
+
+type ipAddressWithPrefix string // ipAddressWithPrefix is the IP address with the prefix separated with an underscore
+type stickySessionMemoryEntry struct {
+	Host        *Host
+	LastUsed    time.Time
+	ExpireTimer *time.Timer
 }
 
 func NewLoadBalancer(distributionType DistributionType, forceHTTPS bool, sticky bool) *LoadBalancer {
@@ -29,12 +44,15 @@ func NewLoadBalancer(distributionType DistributionType, forceHTTPS bool, sticky 
 	if err != nil {
 		panic(err)
 	}
-	return &LoadBalancer{
+	logChan := make(chan LogEntry, 1024)
+	lb := &LoadBalancer{
 		DomainHosts:      make(map[string][]Host),
 		DistributionType: distributionType,
 		ForceHTTPS:       forceHTTPS,
 		Sticky:           sticky,
+		LogChan:          logChan,
 	}
+	return lb
 }
 
 func ParseDistrubutionType(distributionType string) (DistributionType, error) {
@@ -46,4 +64,73 @@ func ParseDistrubutionType(distributionType string) (DistributionType, error) {
 	default:
 		return "", errors.New("invalid distribution type")
 	}
+}
+
+func (lb *LoadBalancer) GetStickySessionHost(ipAddress string, prefix string) *Host {
+	key := ipAddressWithPrefix(ipAddress + "_" + prefix)
+	lb.StickySessionMemoryMu.Lock()
+	defer lb.StickySessionMemoryMu.Unlock()
+	entry, ok := lb.StickySessionMemory[key]
+	if !ok {
+		return nil
+	}
+	return entry.Host
+}
+
+func (lb *LoadBalancer) SetStickySessionHost(ipAddress string, prefix string, host *Host) error {
+	key := ipAddressWithPrefix(ipAddress + "_" + prefix)
+	lb.StickySessionMemoryMu.Lock()
+	defer lb.StickySessionMemoryMu.Unlock()
+	entry, ok := lb.StickySessionMemory[key]
+	if !ok {
+		timer := time.NewTimer(time.Duration(lb.StickySessionResetInterval) * time.Second)
+		entry = stickySessionMemoryEntry{
+			Host:        host,
+			LastUsed:    time.Now(),
+			ExpireTimer: timer,
+		}
+		go lb.expireStickySession(key)
+	} else {
+		entry.LastUsed = time.Now()
+		entry.ExpireTimer.Reset(time.Duration(lb.StickySessionResetInterval) * time.Second)
+	}
+	return nil
+}
+
+func (lb *LoadBalancer) expireStickySession(key ipAddressWithPrefix) {
+	entry, ok := lb.StickySessionMemory[key]
+	if !ok {
+		lb.LogChan <- LogEntry{
+			Type:    LogTypeError,
+			Message: "Sticky session memory entry not found",
+		}
+		return
+	}
+	<-entry.ExpireTimer.C
+	lb.StickySessionMemoryMu.Lock()
+	defer lb.StickySessionMemoryMu.Unlock()
+	delete(lb.StickySessionMemory, key)
+}
+
+// Starts a goroutine that listens on the log channel and logs the messages with an optional writer
+func (lb *LoadBalancer) StartLogger(w *io.WriteCloser) {
+	go func() {
+		if w != nil {
+			defer (*w).Close()
+		}
+		for {
+			entry, ok := <-lb.LogChan
+			if !ok {
+				log.Println("SYSTEM: Log channel closed")
+				if w != nil {
+					(*w).Write([]byte("SYSTEM: Log channel closed"))
+				}
+				return
+			}
+			log.Println(string(entry.Type) + " : " + entry.Message)
+			if w != nil {
+				(*w).Write([]byte(string(entry.Type) + " : " + entry.Message))
+			}
+		}
+	}()
 }
